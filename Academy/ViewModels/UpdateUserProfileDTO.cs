c52b3370cb9ac65c25aaceb0f259326978f795ec
Ayamahmoud359 <Ayamaya359@gmail.com>
2024-11-23 15:26:10 +0200

    commit

---
+++ Academy/ViewModels/UpdateUserProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Academy.ViewModels
+{
+    public class UpdateUserProfileDTO
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+    }
+}
