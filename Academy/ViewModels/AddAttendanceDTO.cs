b25c1ca5fc14e8878ba402a1312a2e13ceba914d
MohamedFayed11 <ms2259@fayoum.edu.eg>
2024-11-24 12:02:49 +0200

    update

---
+++ Academy/ViewModels/AddAttendanceDTO.cs
@@ -0,0 +1,15 @@
+﻿using Academy.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.ViewModels
+{
+    public class AddAttendanceDTO
+    {
+       
+        public bool IsAbsent { get; set; }
+        public DateTime? Date { get; set; }
+        public string? Type { get; set; }
+        public int SubscriptionId { get; set; }
+        public int? TrainerId { get; set; }
+    }
+}
