4a565aeb17de4e378d99e997e3018eb232951a2c
MohamedFayed11 <ms2259@fayoum.edu.eg>
2024-11-28 04:49:19 +0200

    update

--- Academy/Areas/Admin/Pages/Index.cshtml.cs
+++ Academy/Areas/Admin/Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using Academy.Data;
+using Academy.Models;
+using Academy.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,30 @@ namespace Academy.Areas.Admin.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly AcademyContext _context;
+
+        public HomeStatistcsVM HomeStatistcsVM { get; set; }
+
+
+        public IndexModel(AcademyContext context)
+        {
+            _context = context;
+            HomeStatistcsVM = new HomeStatistcsVM();
+        }
+
         public void OnGet()
         {
+            HomeStatistcsVM = new HomeStatistcsVM
+            {
+                BranchesCount = _context.Branches.Where(e=> e.IsActive && e.IsDeleted == false).Count(),
+                TrainersCount = _context.Trainers.Where(e => e.IsActive && e.IsDeleted == false).Count(),
+                TraineesCount = _context.Trainees.Where(e => e.IsActive && e.IsDeleted == false).Count(),
+                ParentsCount = _context.Parents.Where(e => e.IsActive && e.IsDeleted == false).Count(),
+                CompetitionTeamsCount = _context.CompetitionTeam.Where(e => e.IsActive && e.IsDeleted == false).Count(),
+                ChampionsCount = _context.Champions.Where(e => e.IsDeleted == false).Count()
+            };
+
+
         }
     }
 }
---
+++ Academy/ViewModels/HomeStatistcsVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Academy.ViewModels
+{
+    public class HomeStatistcsVM
+    {
+        public int BranchesCount { get; set; }
+        public int TrainersCount { get; set; }
+        public int TraineesCount { get; set; }
+        public int ParentsCount { get; set; }
+        public int CompetitionTeamsCount { get; set; }
+        public int ChampionsCount { get; set; }
+    }
+}
