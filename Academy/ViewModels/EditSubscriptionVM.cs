41af4acd539b0f125b6b9822c7ccb65f8cf065ef
MohamedFayed11 <ms2259@fayoum.edu.eg>
2024-11-30 10:14:39 +0200

    update

---
+++ Academy/ViewModels/EditSubscriptionVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Academy.ViewModels
+{
+    public class EditSubscriptionVM
+    {
+        public int SubscriptionId { get; set; }
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+        public bool? IsActive { get; set; }
+        
+
+    }
+}
